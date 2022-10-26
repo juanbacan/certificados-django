3c7267d60b2ca2414ef20ba8bb109e8759c55784
juanbacan <juan.ingaor@gmail.com>
2022-10-26 14:57:39 -0500

    url certificados

--- applications/certificados/urls.py
+++ applications/certificados/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.CertificadosView.as_view(), name='index'),
+    path('', views.CertificadosView.as_view(), name='certificados'),
     path('upload_certificados', views.UploadCertificadosView.as_view(), name='upload_certificados'),
     path('verificar/', views.VerificarCertificadoView.as_view(), name='verificar_certificado'),
     path('imprimir_certificado/<str:codigo>', views.ImprimirCertificado.as_view(), name='imprimir_certificado'),
